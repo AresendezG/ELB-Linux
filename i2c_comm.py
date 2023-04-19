0738b15670ffd627c1519367ded1c52ea4be98c6
Alejandro Resendez <al.resendez.gro@gmail.com>
2023-04-19 16:42:34 -0700

    Add read voltage fnc

--- i2c_comm.py
+++ i2c_comm.py
@@ -1,4 +1,4 @@
-from asyncio import sleep
+import asyncio
 import time
 from smbus2 import SMBus
 from i2c_types import LedMode
@@ -37,26 +37,57 @@ class ELB_Linux:
 
     def __get_uut_sn(self) -> list:
         # write page 0
-        self.bus.write_i2c_block_data(self.DEV_ADD, 127, [0])  
+        self.bus.write_i2c_block_data(self.DEV_ADD, 127, [0])
+        # read SN from reg166  
         retdata = self.bus.read_i2c_block_data(self.DEV_ADD, 166, 16)
-        serial_number = [x for x in retdata] #array that holds the serialnumber
+        serial_array = [x for x in retdata] #array that holds the serialnumber
+        serial_str = "".join(chr(x) for x in serial_array)
+        # read PN from reg148
         retdata = self.bus.read_i2c_block_data(self.DEV_ADD, 148, 16)
-        part_number = [x for x in retdata] #array that holds the part number
+        pn_array = [x for x in retdata] #array that holds the part number
+        part_number = "".join(chr(x) for x in pn_array)
+        # read revision from reg164
         retdata = self.bus.read_i2c_block_data(self.DEV_ADD, 164, 2)
         revision = [x for x in retdata] # array that holds the rev number
-        return [serial_number, part_number, revision]
+        return [serial_str, part_number, revision]
+    
+    # Function to test the temperature 
+    def ReadTempFnc(self, regaddress: int) -> float:
+         #Can be refactored too
+        retdata = self.bus.read_i2c_block_data(self.DEV_ADD, regaddress, 2)
+        retdata = [x for x in retdata]
+        temp = ((retdata[0]<<8) + retdata[1])/256 #temp ADC is most likely 256 levels? 
+        return temp
+    
+    # Function to read voltages
+    def ReadVoltageFnc(self, regaddress: int) -> float:
+        #Read a voltage level and log to the test results file
+        retdata = self.bus.read_i2c_block_data(self.DEV_ADD, regaddress, 2)
+        retdata = [x for x in retdata]
+        # Get the voltage level from the given registry
+        vcc = ((retdata[0]<<8) + retdata[1])*0.0001
+        return vcc
 
-    def led_sequence(self):
+
+    async def led_sequence(self):
         # write page 3
         self.bus.write_i2c_block_data(self.DEV_ADD, 127, [3])
         # flash LEDs
         self.bus.write_i2c_block_data(self.DEV_ADD, 129, LedMode.BOTH_FLASH)
-        sleep(5)
+        await asyncio.sleep(5)
         self.bus.write_i2c_block_data(self.DEV_ADD, 129, LedMode.GREEN_ON)
-        sleep(5)
+        await asyncio.sleep(5)
         self.bus.write_i2c_block_data(self.DEV_ADD, 129, LedMode.RED_ON)
-        sleep(5)
-        self.bus.write_i2c_block_data(self.DEV_ADD, 129, LedMode.LED_OFF)        
+        await asyncio.sleep(5)
+        self.bus.write_i2c_block_data(self.DEV_ADD, 129, LedMode.LED_OFF)
+
+    def VoltageSequence(self):
+        print("VCC Voltage: ")
+        vcc = self.ReadVoltageFnc(168)
+        print(vcc)
+        print("VBATT Voltage")
+        vbatt = self.ReadVoltageFnc(180)
+        print(vbatt)        
 
 
     def main(self):
@@ -68,7 +99,8 @@ class ELB_Linux:
         [serial_number, part_number, revision] = self.__get_uut_sn()
         print("Serial Number: ",serial_number)
         print("Part Number: ",part_number)
-        print("Revision: ",revision)        
+        print("Revision: ",revision)
+        self.VoltageSequence()        
         print("LEDs Routine")
         self.led_sequence()
         # bus = SMBus()
