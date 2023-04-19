50f751b7ed8d471c7c253f5399633037c0b87ed6
Alejandro Resendez <al.resendez.gro@gmail.com>
2023-04-19 16:00:58 -0700

    Updates to the i2c syntax and new LED sequence

--- i2c_comm.py
+++ i2c_comm.py
@@ -1,22 +1,27 @@
+from asyncio import sleep
+import time
 from smbus2 import SMBus
+from i2c_types import LedMode
 
 class ELB_Linux:
-    #define port number
+    #-- Class variables definitions
+    #define i2c BUS to be used
     DEVICE_BUS = 1
     DEV_ADD = 0x50
+    #define bus object
     bus = None
 
     #function declaration
 
     def __init__(self) -> None:
         print("Init SMBus")
-        self.bus = SMBus(self.DEV_ADD)
+        self.bus = SMBus(self.DEVICE_BUS)
         print("i2c Bus started at address: {}",self.DEV_ADD)
         pass
 
     def __checfw_ver(self) -> list:
         # Write Page 3
-        self.bus.write_byte_data(self.DEV_ADD, 127, 3)        
+        self.bus.write_i2c_block_data(self.DEV_ADD, 127, [3])        
         # Retrieve data from registers
         retdata = self.bus.read_i2c_block_data(self.DEV_ADD, 182, 4)
         retdata = [x for x in retdata]
@@ -32,7 +37,7 @@ class ELB_Linux:
 
     def __get_uut_sn(self) -> list:
         # write page 0
-        self.bus.write_byte_data(self.DEV_ADD, 127, 0)  
+        self.bus.write_i2c_block_data(self.DEV_ADD, 127, [0])  
         retdata = self.bus.read_i2c_block_data(self.DEV_ADD, 166, 16)
         serial_number = [x for x in retdata] #array that holds the serialnumber
         retdata = self.bus.read_i2c_block_data(self.DEV_ADD, 148, 16)
@@ -41,25 +46,31 @@ class ELB_Linux:
         revision = [x for x in retdata] # array that holds the rev number
         return [serial_number, part_number, revision]
 
-    def led_ctrl(self):
+    def led_sequence(self):
         # write page 3
-        self.bus.write_byte_data(self.DEV_ADD, 127, 3)
+        self.bus.write_i2c_block_data(self.DEV_ADD, 127, [3])
         # flash LEDs
-        self.bus.write_byte_data(self.DEV_ADD, 129, 0x22)        
+        self.bus.write_i2c_block_data(self.DEV_ADD, 129, LedMode.BOTH_FLASH)
+        sleep(5)
+        self.bus.write_i2c_block_data(self.DEV_ADD, 129, LedMode.GREEN_ON)
+        sleep(5)
+        self.bus.write_i2c_block_data(self.DEV_ADD, 129, LedMode.RED_ON)
+        sleep(5)
+        self.bus.write_i2c_block_data(self.DEV_ADD, 129, LedMode.LED_OFF)        
 
 
     def main(self):
         print("Getting the FWversion:")
         [dsp_ver, dsp_id, dsp_rev] = self.__checfw_ver()
-        print("DSP Version {}",dsp_ver)
-        print("DSP ID: {}", dsp_id)
+        print("DSP Version: ",dsp_ver)
+        print("DSP ID: ", dsp_id)
         print("Getting UUT's SN: ")
         [serial_number, part_number, revision] = self.__get_uut_sn()
-        print("Serial Number: {}",serial_number)
-        print("Part Number: {}",part_number)
-        print("Revision: {}",revision)
-        print("Flash LEDs")
-        self.led_ctrl()
+        print("Serial Number: ",serial_number)
+        print("Part Number: ",part_number)
+        print("Revision: ",revision)        
+        print("LEDs Routine")
+        self.led_sequence()
         # bus = SMBus()
 
 
---
+++ i2c_types.py
@@ -0,0 +1,9 @@
+class LedMode:
+	#reg is 129
+	RED_ON         =[0x01]       
+	GREEN_ON       =[0x10]     
+	RED_FLASH      =[0x02]        
+	GREEN_FLASH    =[0x20]
+	BOTH_ON        =[0x11]   
+	BOTH_FLASH     =[0x22]
+	LED_OFF        =[0x00]
