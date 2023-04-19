65bc89112bed663a4731b5139de6d3d090a3db2e
Alejandro Resendez <al.resendez.gro@gmail.com>
2023-04-19 11:12:17 -0700

    fix the instance sintax error

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from smbus2 import SMBus
 from i2c_comm import ELB_Linux
 
-print("Test Script Main")
-ELB_Linux.main()
+i2c_comm = ELB_Linux()
+
+print("--- ELB Testing Script --- ")
+i2c_comm.main()
