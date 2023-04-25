0df450801dbc86195880bba37583d995ae97eac0
Alejandro Resendez <al.resendez.gro@gmail.com>
2023-04-25 14:54:46 -0700

    Add the log function to files

---
+++ log_management.py
@@ -0,0 +1,82 @@
+import os
+import datetime
+
+class LOG_Manager():
+    
+    def __init__(self, uut_serial: str, def_path: str) -> bool:
+        # Var definitions
+        self.default_path = def_path
+        self.logfile = None     # Handler of the plaintxt logfile
+        self.resultsfile = None # Handler of the results logfile
+        self.logfilename = ""
+        self.ext_logfile = ".txt"
+        self.ext_results = ".csv"
+        self.serial = uut_serial        
+        self.logfiletime = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        # Validate serial number
+        if (not isinstance(uut_serial, str) or len(uut_serial) < 1):
+            raise TypeError("ERROR: Expected a Serial Number as Parameter")        
+        dir_status = self.__validate_def_path()
+        files_status = self.__OpenLogFiles()
+        self.__init_logfile()
+        return (dir_status and files_status)
+    
+    def __del__(self):
+        self.logfile.close()
+        self.resultsfile.close()
+        pass
+
+    # Validate default paths
+    def __validate_def_path(self) -> bool:
+        if not os.path.exists(self.default_path):
+        # Try to create the default dir if it does not exist
+            try:
+                os.makedirs(self.default_path)
+                return True
+        # for whatever reason, OS has rejected the creation of the default dir    
+            except:
+                return False
+        else:
+            return True 
+
+    def __OpenLogFiles(self) -> bool:
+        # Update the results logfile
+        self.logfilename = "ELB_"+self.serial+"_"+self.logfiletime+self.ext_logfile
+        self.resultsfilename = "ELB_"+self.serial+"_"+self.logfiletime+self.ext_results
+
+        # check if file exists
+        filecnt = 0
+        while os.path.isfile(self.default_path+self.logfilename):
+            filecnt = filecnt + 1
+            # print warning
+            print("Warning: File already exist: {}\n".format(self.default_path+self.logfilename))
+            # create unique file name
+            self.logfilename = "ELB_"+self.serial+"_"+self.logfiletime+"_"+str(filecnt)+self.ext_logfile
+            self.resultsfilename = "ELB_"+self.serial+"_"+self.logfiletime+"_"+str(filecnt)+self.ext_results
+        
+        # create files
+        print("Event: Opening {}\n".format(self.default_path+self.logfilename))
+        print("Event: Opening {}\n".format(self.default_path+self.resultsfilename))
+        try: 
+            self.logfile = open(self.default_path+self.logfilename, "w")
+            self.resultsfile = open(self.default_path+self.resultsfilename, "w")
+            return True
+        except:
+            return False
+
+    def __init_logfile(self):
+        self.logfile("Juniper Electric Loopback Test Log\n")
+        self.logfile("=====================\n")
+        self.logfile("Test Date: {}\n".format(self.logfiletime))
+        self.resultsfile("Juniper Electric Loopback TestResults\n")
+        self.logfile("Test Date,{}\n".format(self.logfiletime))
+        self.logfile("Result,Test Name,High Limit,Measurement,Low Limit\n")
+
+
+    def logtofile(self, line: str):
+        self.logfile.write(line)
+        return
+
+    def logresult(self, result: str, testname:str, measurement:str, highlim: str, lowlim: str):
+        self.resultsfile.write("{},{},{},{},{}\n".format(result,testname,highlim,measurement,lowlim))
+        pass
--- main.py
+++ main.py
@@ -1,41 +1,23 @@
 from smbus2 import SMBus
 from i2c_comm import ELB_i2c
+from log_management import LOG_Manager
+
+# Change these variables for user settings
+default_path = "~/elb_results/"
+uut_serial = "ZP3923110084"
 
 i2c_comm = ELB_i2c()
+logs = LOG_Manager(uut_serial, default_path)
 
-print("--- ELB Test Script --- ")
+# Run the test only if the log can be created!
+if (logs):
+    print("--- ELB Test Script --- ")
+    print("Calling GPIO Test")
+    gpio_results = i2c_comm.Test_GPIO_all()
+    for rsl in range(gpio_results.count()):
+        logs.logtofile("GPIO Result: {}".format(gpio_results[rsl]))
 
-print("Calling GPIO Test")
-gpio_results = i2c_comm.Test_GPIO_all()
-for rsl in range(gpio_results.count()):
-    print("GPIO Result {}")
-
-print("Calling Current Test")
-current_results = i2c_comm.CurrentSequence()
-
-
-
-'''
-    def main(self):
-        print("Getting the FWversion:")
-        [dsp_ver, dsp_id, dsp_rev, fwver] = self.__checfw_ver()
-        print("DSP Version: ",dsp_ver)
-        print("DSP ID: ", dsp_id)
-        print("Getting UUT's SN: ")
-        [serial_number, part_number, revision] = self.__get_uut_sn()
-        print("Serial Number: ",serial_number)
-        print("Part Number: ",part_number)
-        print("Revision: ",revision)
-        print("Firmware version: {}.{}".format(fwver[0], fwver[1]))
-        self.Get_AllVoltages()
-        self.Get_AllTemps()
-        print("ePPS Data:")
-        [freq, duty, dutyms] = self.Get_Alll_EPPSData()
-        print("ePPS Frequency: ",freq)
-        print("ePPS Duty %",duty)
-        print("ePPS Duty ms",dutyms)
-        self.Get_Alll_EPPSData()        
-        print("LEDs Routine")
-        self.led_sequence()
-        # bus = SMBus()
-'''
+    print("Calling Current Test")
+    current_results = i2c_comm.CurrentSequence()
+else:
+    print("ERROR: Unable to create logfiles. Test stopped")
