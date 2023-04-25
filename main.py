a9405e3fbc8cf145cf3ee78346a5e02e7f1b91a9
Alejandro Resendez <al.resendez.gro@gmail.com>
2023-04-25 15:00:58 -0700

    Enable log to file fnc

--- main.py
+++ main.py
@@ -11,57 +11,31 @@ logs = LOG_Manager(uut_serial, default_path)
 
 # Run the test only if the log can be created!
 if (logs):
-    print("--- ELB Test Script --- ")
-    print("Calling GPIO Test")
+
+    logs.logtofile("--- ELB Test Script --- ")
+    logs.logtofile("Calling GPIO Test")
     gpio_results = i2c_comm.Test_GPIO_all()
     for rsl in range(gpio_results.count()):
         logs.logtofile("GPIO Result: {}".format(gpio_results[rsl]))
 
-print("Event: Calling GPIO Test")
-gpio_results = i2c_comm.Test_GPIO_all()
-for rsl in range(len(gpio_results)):
-    print("GPIO Result: {}".format(gpio_results[rsl]))
+    logs.logtofile("Event: Calling GPIO Test")
+    gpio_results = i2c_comm.Test_GPIO_all()
+    for rsl in range(len(gpio_results)):
+        logs.logtofile("GPIO Result: {}".format(gpio_results[rsl]))
 
+    logs.logtofile("Event: Calling Insertion Counter")
+    [ins_accum, ins_nibb] = i2c_comm.Get_InsertionCount()
+    logs.logtofile("Accumulate: ",ins_accum)
+    logs.logtofile("Nibble: ",ins_nibb)  
+    logs.logtofile("Event: Calling Read Voltage Sensors")
+    volt_results = i2c_comm.Get_AllVoltages()
 
-print("Event: Calling Insertion Counter")
-[ins_accum, ins_nibb] = i2c_comm.Get_InsertionCount()
-print("Accumulate: ",ins_accum)
-print("Nibble: ",ins_nibb)
+    logs.logtofile("Event: Calling the Temp Sensor reading")
+    temp_results = i2c_comm.Get_AllTemps()
+    i2c_comm.Get_UUT_SN()
+    logs.logtofile("Calling Current Test")
+    current_results = i2c_comm.CurrentSequence()
+    logs.logtofile(current_results)
 
-print("Event: Calling Read Voltage Sensors")
-volt_results = i2c_comm.Get_AllVoltages()
-
-print("Event: Calling the Temp Sensor reading")
-temp_results = i2c_comm.Get_AllTemps()
-
-i2c_comm.Get_UUT_SN()
-print("Calling Current Test")
-current_results = i2c_comm.CurrentSequence()
-print(current_results)
-
-
-
-'''
-    def main(self):
-        print("Getting the FWversion:")
-        [dsp_ver, dsp_id, dsp_rev, fwver] = self.__checfw_ver()
-        print("DSP Version: ",dsp_ver)
-        print("DSP ID: ", dsp_id)
-        print("Getting UUT's SN: ")
-        [serial_number, part_number, revision] = self.__get_uut_sn()
-        print("Serial Number: ",serial_number)
-        print("Part Number: ",part_number)
-        print("Revision: ",revision)
-        print("Firmware version: {}.{}".format(fwver[0], fwver[1]))
-        self.Get_AllVoltages()
-        self.Get_AllTemps()
-        print("ePPS Data:")
-        [freq, duty, dutyms] = self.Get_Alll_EPPSData()
-        print("ePPS Frequency: ",freq)
-        print("ePPS Duty %",duty)
-        print("ePPS Duty ms",dutyms)
-        self.Get_Alll_EPPSData()        
-        print("LEDs Routine")
-        self.led_sequence()
-        # bus = SMBus()
-'''
+else:
+    print("ERROR: Unable to create Log Files")
