ef9bc00ee6a4cb91e9bb9140d983a9e9934876b7
Alejandro Resendez <al.resendez.gro@gmail.com>
2023-04-26 11:37:38 -0700

    fix the main fnc but just as a demo

--- main.py
+++ main.py
@@ -10,27 +10,32 @@ i2c_comm = ELB_i2c()
 try:
     # Run tests only if Logfiles can be created!
     logs = LOG_Manager(uut_serial, default_path)
-
     logs.logtofile("--- ELB Test Script --- ")
-
+    
     logs.logtofile("Event: Calling GPIO Test")
     gpio_results = i2c_comm.Test_GPIO_all()
     for rsl in range(len(gpio_results)):
         logs.logtofile("GPIO Result: {}".format(gpio_results[rsl]))
 
     logs.logtofile("Event: Reading UUT SN")
-    i2c_comm
+    [serial_str, part_number, revision] = i2c_comm.Get_UUT_SN()
+    logs.logtofile("UUT SerialNum: {}".format(serial_str))
+    logs.logtofile("Part Number: {}".format(part_number))
+    logs.logtofile("Revision: {}".format(revision))
 
     logs.logtofile("Event: Calling Insertion Counter")
     [ins_accum, ins_nibb] = i2c_comm.Get_InsertionCount()
     logs.logtofile("Accumulate: {}".format(ins_accum))
-    logs.logtofile("Nibble: ",ins_nibb)  
+    logs.logtofile("Nibble: {}".format(ins_nibb))  
     logs.logtofile("Event: Calling Read Voltage Sensors")
     volt_results = i2c_comm.Get_AllVoltages()
+    for rsl in range(len(volt_results)):
+        logs.logtofile("Voltage Sensor_{} Result: {}".format(rsl, volt_results[rsl]))
 
     logs.logtofile("Event: Calling the Temp Sensor reading")
     temp_results = i2c_comm.Get_AllTemps()
-    i2c_comm.Get_UUT_SN()
+    for rsl in range(len(temp_results)):
+        logs.logtofile("Temp Sensor_{} Result: {}".format(rsl, temp_results[rsl]))
     logs.logtofile("Calling Current Test")
     current_results = i2c_comm.CurrentSequence()
     logs.logtofile(current_results)
