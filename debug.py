ec7111ce25ea73f266af5d419227e79dbc0a5694
Alejandro Resendez <al.resendez.gro@gmail.com>
2023-04-29 15:31:49 -0700

    Implementations for i2c host debug using Rpico

--- debug.py
+++ debug.py
@@ -3,7 +3,7 @@ import sys
 import xml
 from Sequencer import SeqConfig
 import sys
-#from smbus2 import SMBus
+import time
 #from i2c_comm import ELB_i2c
 #from i2c_types import MOD_Rates
 from log_management import LOG_Manager
@@ -25,3 +25,36 @@ class DummyClass:
     def power_loads(self) -> list:
         print("Test:\tExecuting PowerLoad Test")
         return [0.2, 0.3, 0.5]
+    
+
+class i2c_PicoInterface:
+
+    def __init__(self) -> None:
+        from smbus2 import SMBus
+        self.pico_address = 0x17
+        self.RPI_BUS = 1
+        self.i2cbus = SMBus(self.RPI_BUS)
+        print("Opening Comm with RPI Pico at {}".format(self.pico_address))
+
+
+    def read_sn(self) -> str:
+        print("Event:\tTrying to read Pico SN")
+        self.i2cbus.write_i2c_block_data(self.pico_address, 127, [3])
+        print("check for cmd ack status...")
+        time.sleep(5)
+        data = self.i2cbus.read_i2c_block_data(self.pico_address, 166, 8)
+        serial_array = [x for x in data] #array that holds the serialnumber
+        serial_str = "".join(chr(x) for x in serial_array)
+        print("Serial: {}".format(serial_str))
+        return serial_str
+    
+    def read_fw_ver(self) -> str:
+        print("Event:\tTrying to read Pico FW Version")
+        self.i2cbus.write_i2c_block_data(self.pico_address, 127, [3])
+        print("check for cmd ack status...")
+        time.sleep(5)
+        data = self.i2cbus.read_i2c_block_data(self.pico_address, 39, 2)
+        fwver = "{}.{}".format(data[0], data[1])
+        print("FW Version: {}".format(fwver))
+        return fwver
+
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import sys
 from log_management import LOG_Manager
 from program_manager import ProgramControl
+from debug import i2c_PicoInterface
 
+#Main = ProgramControl(sys.argv)
+#Main.run_program()
 
-Main = ProgramControl(sys.argv)
-Main.run_program()
+print("Debug i2c communication with Pico")
+pico_debug = i2c_PicoInterface()
+pico_debug.read_fw_ver()
+pico_debug.read_sn()
